908f6aa61af0623a2edf5d2fa87af6c420de7451
Noriaki Tatsumi <noriaki.tatsumi@blackboard.com>
2014-05-07 14:06:37 -0400

    Metric categorization bug fix

--- src/blackboard/monitor/newrelic/postgresql/PostgresqlData.java
+++ src/blackboard/monitor/newrelic/postgresql/PostgresqlData.java
@@ -8,18 +8,18 @@ import java.util.Map;
  * Created by ntatsumi on 5/6/14.
  */
 public enum PostgresqlData {
-    TOTAL_CONNECTIONS("Connections/TotalConnections", "connections", "Total Connections"),
-    MAX_CONNECTIONS("Connections/MaxConnections", "connections", "Max Connections"),
-    TOTAL_SESSION_TIME_HOURS("Sessions/TotalSessionTimeHours", "hours", "Total Session Time"),
-    OLDEST_SESSION_TIME_HOURS("Sessions/OldestSessionTimeHours", "hours", "Oldest Session Duration"),
-    DATABASE_SIZE("Storage/DatabaseSize", "MB", "Database Size"),
-    TOTAL_LIVE_SIZE("Storage/TotalLiveSize", "MB", "Total Live Size"),
-    TOTAL_DEAD_SIZE("Storage/TotalDeadSize", "MB", "Total Dead Size"),
-    TOTAL_TABLE_SCANS("AccessPatterns/TotalTableScans", "MB", "Total Table Scans"),
-    TOTAL_INDEX_SCANS("AccessPatterns/TotalIndexScans", "MB", "Total Index Scans"),
-    TOTAL_INSERTS("AccessPatterns/TotalInserts", "MB", "Total Inserts"),
-    TOTAL_UPDATES("AccessPatterns/TotalUpdates", "MB", "Total Updates"),
-    TOTAL_DELETES("AccessPatterns/TotalDeletes", "MB", "Total Deletes");
+    TOTAL_CONNECTIONS("TotalConnections", "connections", "Connections/Total Connections"),
+    MAX_CONNECTIONS("MaxConnections", "connections", "Connections/Max Connections"),
+    TOTAL_SESSION_TIME_HOURS("TotalSessionTimeHours", "hours", "Sessions/Total Session Time"),
+    OLDEST_SESSION_TIME_HOURS("OldestSessionTimeHours", "hours", "Sessions/Oldest Session Duration"),
+    DATABASE_SIZE("DatabaseSize", "MB", "Storage/Database Size"),
+    TOTAL_LIVE_SIZE("TotalLiveSize", "MB", "Storage/Total Live Size"),
+    TOTAL_DEAD_SIZE("TotalDeadSize", "MB", "Storage/Total Dead Size"),
+    TOTAL_TABLE_SCANS("TotalTableScans", "MB", "AccessPatterns/Total Table Scans"),
+    TOTAL_INDEX_SCANS("TotalIndexScans", "MB", "AccessPatterns/Total Index Scans"),
+    TOTAL_INSERTS("TotalInserts", "MB", "AccessPatterns/Total Inserts"),
+    TOTAL_UPDATES("TotalUpdates", "MB", "AccessPatterns/Total Updates"),
+    TOTAL_DELETES("TotalDeletes", "MB", "AccessPatterns/Total Deletes");
 
     private String key;
     private String unit;
